16f3807c02a8e6d95eb723e32bc067f89d3d03e8
Rayan Mejaat <142797698+HandiReaper@users.noreply.github.com>
2025-05-26 13:13:10 +0200

    Finish MenuFilters

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,19 @@
 import { Menu } from "@/src/menu/menu";
+import { REACT_CARDS } from "@/src/react-cards";
 import { Header } from "../src/header/header";
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const resolvedSearchParams = await searchParams;
+  const currentFilter = resolvedSearchParams.filters;
+  const filters = [...new Set(REACT_CARDS.map((c) => c.category))];
+
+  console.log({ currentFilter, filters });
+
   return (
     <main className="m-auto flex h-full max-w-4xl flex-col px-4">
       <Header />
       <div className="mb-4 mt-8 flex flex-1 gap-4 overflow-auto max-lg:flex-col">
-        <Menu />
+        <Menu currentFilter={currentFilter} filters={filters} />
       </div>
     </main>
   );
--- src/menu/menu.jsx
+++ src/menu/menu.jsx
@@ -1,24 +1,38 @@
-export const Menu = () => {
+import clsx from "clsx";
+import Link from "next/link";
+
+export const Menu = ({ filters, currentFilter }) => {
   return (
-    <nav className="flex w-full flex-wrap gap-4 lg:max-w-[200px] lg:flex-col">
-      <a className="rounded-md px-2 py-1 capitalize transition-colors hover:bg-gray-200">
+    <aside className="flex w-full flex-wrap gap-4 lg:max-w-[200px] lg:flex-col">
+      <MenuItem isActive={!filters} filter="" key="All">
         All
-      </a>
-      <a className="rounded-md px-2 py-1 capitalize transition-colors hover:bg-gray-200">
-        Components
-      </a>
-      <a className="rounded-md px-2 py-1 capitalize transition-colors hover:bg-gray-200">
-        Hooks
-      </a>
-      <a className="rounded-md px-2 py-1 capitalize transition-colors hover:bg-gray-200">
-        State-Manager
-      </a>
-      <a className="rounded-md px-2 py-1 capitalize transition-colors hover:bg-gray-200">
-        Server
-      </a>
-      <a className="rounded-md px-2 py-1 capitalize transition-colors hover:bg-gray-200">
-        Patterns
-      </a>
-    </nav>
+      </MenuItem>
+      {filters.map((filter) => (
+        <MenuItem
+          filter={filter}
+          key={filter}
+          isActive={filter === currentFilter}
+        >
+          {filter}
+        </MenuItem>
+      ))}
+    </aside>
+  );
+};
+
+const MenuItem = ({ children, filter, isActive }) => {
+  return (
+    <Link
+      href={`/?filters=${filter}`}
+      key={filter}
+      className={clsx(
+        "rounded-md px-2 py-1 capitalize transition-colors hover:bg-gray-200",
+        {
+          "font-bold": isActive,
+        }
+      )}
+    >
+      {children}
+    </Link>
   );
 };
